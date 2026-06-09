187da952866e84e5aca43dff8bd30ca52146fbe9
Yunzhe Jiang <yunzhe.jiang@duke.edu>
2026-06-09 17:13:09 +0800

    modify & debug chapter8

---
+++ chapter8_making_pizzas.py
@@ -0,0 +1,5 @@
+import chapter8_pizza
+
+chapter8_pizza.make_pizza(16, 'pepperoni')
+
+chapter8_pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
---
+++ chapter8_pizza.py
@@ -0,0 +1,7 @@
+def make_pizza(size, *toppings):
+
+    print(f"\nMaking a {size}-inch pizza with:")
+
+    for topping in toppings:
+
+        print("- " + topping)
